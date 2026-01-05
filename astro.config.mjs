273af560ce1fcfc04ab5075804764cb8e53a27a0
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-01-05 19:07:31 +0100

    Refactor astro.config.mjs to consolidate configuration and simplify vercel.json redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,10 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  base: '/spacex',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: vercel(),
-  base: '/spacex',
 });
